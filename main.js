7300260e227a8be8ac3d49af1b4f57db81a234fb
Md. Shakib Mia <smdshakibmia2001@gmail.com>
2024-07-15 15:06:04 +0600

    testimonials are done

--- main.js
+++ main.js
@@ -112,3 +112,29 @@ checkIsInViewport("#value1", (isInViewPort) => {
     document.getElementById("value2").innerText = 0;
   }
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  var swiper = new Swiper(".swiper-container", {
+    // slidesPerView: 2,
+    autoplay: true,
+    // spaceBetween: 68,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 1,
+        // spaceBetween: 20,
+      },
+      1024: {
+        slidesPerView: 2,
+        // spaceBetween: 50,
+      },
+    },
+    // navigation: {
+    //   nextEl: ".swiper-button-next",
+    //   prevEl: ".swiper-button-prev",
+    // },
+  });
+});
